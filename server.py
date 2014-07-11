e93a0aa4d544c820ee9ab1940a3c848863f30abe
Michael Lat <mikelat@gmail.com>
2014-07-10 21:46:10 -0700

    db support, better chat

--- server.py
+++ server.py
@@ -1,39 +1,56 @@
-from urllib.urlparse import urlparse, parse_qsl
+from urllib.parse import parse_qsl
+from datetime import datetime
 import tornado.httpserver
 import tornado.websocket
 import tornado.ioloop
 import tornado.web
+import sqlite3
+from time import time
+import cgi
 import os
-
 PATH = os.path.dirname(os.path.abspath(__file__))
 DEBUG = True
+VALID_COLOURS = ["black", "blue", "red", "orange", "green", "purple", "brown"]
 sockets = []
+db = sqlite3.connect('chat.db')
 
 class WSHandler(tornado.websocket.WebSocketHandler):
 	def open(self):
-		print('new connection', self)
-		self.write_message("Hello World")
 		sockets.append(self)
 
-	def on_message(self, message):
-		print('message received %s' % parse_qsl(urlparse(message)[4]))
+	def on_message(self, ws_message):
+		ws_data = dict(parse_qsl(ws_message))
+		if "name" not in ws_data:
+			return
+		if "message" not in ws_data:
+			ws_data["message"] = ""
+		if ws_data["colour"] not in VALID_COLOURS:
+			ws_data["colour"] = VALID_COLOURS[0]
+		message = cgi.escape(ws_data["message"])
+		time = "[" + datetime.now().strftime('%H:%M:%S') + "] "
+		c = db.cursor()
+		c.execute("insert into chat (date, name, message) values (?, ?, ?)", [str(int(time())), ws_data["name"], message])
+		db.commit()
+		c.close()
+		for socket in sockets:
+			socket.write_message('<div style="color: ' + ws_data["colour"] + '">' + time + '<strong>' + ws_data["name"] + ':</strong> ' + message + '</div>')
 
 	def on_close(self):
-		print('connection closed')
+		sockets.remove(self)
 
 class WebHandler(tornado.web.RequestHandler):
 	def get(self):
 		self.render("index.html")
 
 application_sockets = tornado.web.Application([
-	(r'/ws', WSHandler),
+	(r"/ws", WSHandler),
 ], debug=DEBUG)
 
 application_web = tornado.web.Application([
-	(r'/fonts/(.*)', tornado.web.StaticFileHandler, {'path': os.path.join(PATH, 'fonts')}),
-	(r'/css/(.*)', tornado.web.StaticFileHandler, {'path': os.path.join(PATH, 'css')}),
-	(r'/js/(.*)', tornado.web.StaticFileHandler, {'path': os.path.join(PATH, 'js')}),
-	(r'/', WebHandler)
+	(r"/fonts/(.*)", tornado.web.StaticFileHandler, {"path": os.path.join(PATH, "fonts")}),
+	(r"/css/(.*)", tornado.web.StaticFileHandler, {"path": os.path.join(PATH, "css")}),
+	(r"/js/(.*)", tornado.web.StaticFileHandler, {"path": os.path.join(PATH, "js")}),
+	(r"/", WebHandler)
 ], debug=DEBUG)
 
 if __name__ == "__main__":
